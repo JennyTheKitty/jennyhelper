e23fa46f7d7e1249e924407d381a95b0f0a23aab
Jenny <JennyTheKitty@protonmail.com>
2021-02-08 17:58:41 +0100

    Make roles more robust

--- src/bot/cogs/roles.py
+++ src/bot/cogs/roles.py
@@ -2,6 +2,7 @@ from typing import Optional, Mapping
 
 import discord
 from discord.ext import commands
+import tortoise
 
 from bot.db.models import RoleMessage
 
@@ -28,9 +29,14 @@ class Roles(commands.Cog):
 
         await ctx.message.delete()
 
-        requested: Mapping[str, discord.Role] = self.get_roles(ctx.guild, msg.content)
+        requested: Mapping[str, int] = self.get_roles(ctx.guild, msg.content)
 
-        await RoleMessage.get_or_create(id=msg.id)
+        print(requested)
+
+        role_message, created = await RoleMessage.get_or_create({"data": requested}, id=msg.id)
+        if not created:
+            role_message.data = requested
+            await role_message.save()
 
         for emoji in requested.keys():
             await msg.add_reaction(emoji)
@@ -38,7 +44,7 @@ class Roles(commands.Cog):
         for emoji in set(str(r) for r in msg.reactions) - set(requested.keys()):
             await msg.clear_reaction(emoji)
 
-    def get_roles(self, guild: discord.Guild, text: str) -> Mapping[str, discord.Role]:
+    def get_roles(self, guild: discord.Guild, text: str) -> Mapping[str, int]:
         roles = {}
 
         for line in text.split("\n"):
@@ -49,37 +55,55 @@ class Roles(commands.Cog):
             for role in guild.roles:
                 if (
                     role.name.lower() == candidate[1]
-                    or role.name.lower().strip(candidate[0]).strip().lower() == candidate[1]
+                    or role.name.lower().strip(candidate[0]).strip().lower()
+                    == candidate[1]
                 ):
-                    roles[candidate[0]] = role
+                    roles[candidate[0]] = role.id
         return roles
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        if not await RoleMessage.exists(id=payload.message_id):
+
+        try:
+            role_message = await RoleMessage.get(id=payload.message_id)
+        except tortoise.exception.DoesNotExist:
+            self.logger(f"No role message for id {payload.message_id}")
             return
+        
+        # msg = await (self.bot.get_channel(payload.channel_id)).fetch_message(
+        #     payload.message_id
+        # )
 
-        msg = await (self.bot.get_channel(payload.channel_id)).fetch_message(
-            payload.message_id
-        )
-
-        roles = self.get_roles(self.bot.get_guild(payload.guild_id), msg.content)
-
-        await payload.member.add_roles(roles[str(payload.emoji)])
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        if not await RoleMessage.exists(id=payload.message_id):
-            return
-
-        msg = await (self.bot.get_channel(payload.channel_id)).fetch_message(
-            payload.message_id
-        )
+        # roles = self.get_roles(self.bot.get_guild(payload.guild_id), msg.content)
 
         guild = self.bot.get_guild(payload.guild_id)
 
-        roles = self.get_roles(guild, msg.content)
+        role_id = role_message.data[str(payload.emoji)]
+
+        role = discord.utils.get(guild.roles, id=role_id)
+
+        await payload.member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        try:
+            role_message = await RoleMessage.get(id=payload.message_id)
+        except tortoise.exception.DoesNotExist:
+            self.logger(f"No role message for id {payload.message_id}")
+            return
+
+        # msg = await (self.bot.get_channel(payload.channel_id)).fetch_message(
+        #     payload.message_id
+        # )
+
+        guild = self.bot.get_guild(payload.guild_id)
+
+        # roles = self.get_roles(guild, msg.content)
 
         member = await guild.fetch_member(payload.user_id)
 
-        await member.remove_roles(roles[str(payload.emoji)])
+        role_id = role_message.data[str(payload.emoji)]
+
+        role = discord.utils.get(guild.roles, id=role_id)
+
+        await member.remove_roles(role)
--- src/bot/db/models.py
+++ src/bot/db/models.py
@@ -4,6 +4,7 @@ from tortoise import fields
 
 class RoleMessage(Model):
     id = fields.BigIntField(pk=True)
+    data = fields.JSONField()
 
     def __str__(self):
         return 'RoleMessage[{}]'.format(self.id)
